31ad4384a1fabcd58a9988d10ad9a3aa9c5121aa
VanHau07 <72301005+VanHau07@users.noreply.github.com>
2024-03-03 18:26:06 +0700

    Add files via upload

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Thu Thương';
+const giftUrl = '#';
+const eventName = 'Chúc Mừng 08/03';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc cô bé mít ướt 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau nữa nhé <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'AnhNen01.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
